c2b5a68649ffa746ac202c3e9cb4c6ff72b3ebf5
Kevin Jhangiani <kevin@soxhub.com>
2015-10-23 21:27:42 +0000

    perf improvements by removing anon fn, still wip

--- jquery.mCustomScrollbar.js
+++ jquery.mCustomScrollbar.js
@@ -1759,6 +1759,49 @@ and dependencies (minified).
 				once=seq.type==="stepped" ? true : false,
 				steplessSpeed=o.scrollInertia < 26 ? 26 : o.scrollInertia, /* 26/1.5=17 */
 				steppedSpeed=o.scrollInertia < 1 ? 17 : o.scrollInertia;
+				
+				// console.log('stepless speed: ' + steplessSpeed);
+				// console.log('stepped speed: ' + steppedSpeed);
+			/* starts sequence */
+			var _on = function(once) {
+				var c=seq.type!=="stepped", /* continuous scrolling */
+					t=s ? s : !once ? 1000/60 : c ? steplessSpeed/1.5 : steppedSpeed, /* timer */
+					m=!once ? 2.5 : c ? 7.5 : 40, /* multiplier */
+					contentPos=[Math.abs(mCSB_container[0].offsetTop),Math.abs(mCSB_container[0].offsetLeft)],
+					ratio=[d.scrollRatio.y>10 ? 10 : d.scrollRatio.y,d.scrollRatio.x>10 ? 10 : d.scrollRatio.x],
+					amount=seq.dir[0]==="x" ? contentPos[1]+(seq.dir[1]*(ratio[1]*m)) : contentPos[0]+(seq.dir[1]*(ratio[0]*m)),
+					px=seq.dir[0]==="x" ? contentPos[1]+(seq.dir[1]*parseInt(seq.scrollAmount)) : contentPos[0]+(seq.dir[1]*parseInt(seq.scrollAmount)),
+					to=seq.scrollAmount!=="auto" ? px : amount,
+					easing=e ? e : !once ? "mcsLinear" : c ? "mcsLinearOut" : "mcsEaseInOut",
+					onComplete=!once ? false : true;
+					
+					// console.log('seq scroll');
+					// console.log(to);
+					// console.log(amount);
+					// console.log(t);
+				//t = 33;
+				//t = 15;
+				//t = 100;
+				t = 26;
+					
+				if(once && t<17){
+					to=seq.dir[0]==="x" ? contentPos[1] : contentPos[0];
+				}
+				_scrollTo(el,to.toString(),{dir:seq.dir[0],scrollEasing:easing,dur:0,onComplete:onComplete});
+				if(once){
+					seq.dir=false;
+					return;
+				}
+				//clearTimeout(seq.step);
+				if (!seq.step) {
+					seq.step = setInterval(_on, t);
+				}
+				// seq.step=setTimeout(function(){
+					// _on();
+				// },t);
+				
+			};
+			
 			switch(action){
 				case "on":
 					seq.dir=[
@@ -1776,34 +1819,11 @@ and dependencies (minified).
 					}
 					break;
 			}
-			/* starts sequence */
-			function _on(once){
-				var c=seq.type!=="stepped", /* continuous scrolling */
-					t=s ? s : !once ? 1000/60 : c ? steplessSpeed/1.5 : steppedSpeed, /* timer */
-					m=!once ? 2.5 : c ? 7.5 : 40, /* multiplier */
-					contentPos=[Math.abs(mCSB_container[0].offsetTop),Math.abs(mCSB_container[0].offsetLeft)],
-					ratio=[d.scrollRatio.y>10 ? 10 : d.scrollRatio.y,d.scrollRatio.x>10 ? 10 : d.scrollRatio.x],
-					amount=seq.dir[0]==="x" ? contentPos[1]+(seq.dir[1]*(ratio[1]*m)) : contentPos[0]+(seq.dir[1]*(ratio[0]*m)),
-					px=seq.dir[0]==="x" ? contentPos[1]+(seq.dir[1]*parseInt(seq.scrollAmount)) : contentPos[0]+(seq.dir[1]*parseInt(seq.scrollAmount)),
-					to=seq.scrollAmount!=="auto" ? px : amount,
-					easing=e ? e : !once ? "mcsLinear" : c ? "mcsLinearOut" : "mcsEaseInOut",
-					onComplete=!once ? false : true;
-				if(once && t<17){
-					to=seq.dir[0]==="x" ? contentPos[1] : contentPos[0];
-				}
-				_scrollTo(el,to.toString(),{dir:seq.dir[0],scrollEasing:easing,dur:t,onComplete:onComplete});
-				if(once){
-					seq.dir=false;
-					return;
-				}
-				clearTimeout(seq.step);
-				seq.step=setTimeout(function(){
-					_on();
-				},t);
-			}
+			
 			/* stops sequence */
 			function _off(){
-				clearTimeout(seq.step);
+				// clearTimeout(seq.step);
+				clearInterval(seq.step);
 				_delete(seq,"step");
 				_stop(el);
 			}
